2a1a7d2455cf77f44f0733e5a5cd3f6fa302cd25
Ramiro Monroy Ramos <ramiro.monroy@udea.edu.co>
2020-05-12 14:57:58 -0500

    Initial commit

---
+++ src/main/java/co/com/udea/butcherprojectapp/butcherprojectapp/ButcherprojectappApplication.java
@@ -0,0 +1,13 @@
+package co.com.udea.butcherprojectapp.butcherprojectapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButcherprojectappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ButcherprojectappApplication.class, args);
+    }
+
+}
